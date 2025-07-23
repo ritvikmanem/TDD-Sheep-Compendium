12ad440a624cbfd9de2e28c41d62091570f63c6b
Ritvik Manem <manemritvik@gmail.com>
2025-07-23 13:13:03 -0400

    feat: added functionality for creating new sheeps

---
+++ models/models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class Sheep(BaseModel):
+    id: int
+    name: str
+    breed: str
+    sex: str
+
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,7 @@ client = TestClient(app)
 
 def test_read_sheep():
     response = client.get("/sheep/1")
+    
     assert response.status_code == 200
 
     assert response.json() == {
@@ -15,3 +16,20 @@ def test_read_sheep():
         "sex": "ewe"
     }
     
+def test_add_sheep():
+    sheep_data_name = {
+        "id": 7,  
+        "name": "Garry",
+        "breed": "Suffolk",
+        "sex": "ewe"
+    }
+    
+    response = client.post("/sheep", json=sheep_data_name)
+       
+    assert response.status_code == 201
+    
+    created_sheep = response.json()
+    assert "id" in created_sheep
+    assert created_sheep["name"] == sheep_data_name["name"]
+    assert created_sheep["breed"] == sheep_data_name["breed"]
+    assert created_sheep["sex"] == sheep_data_name["sex"]
